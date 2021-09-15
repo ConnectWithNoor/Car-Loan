75664f8b460b2cbd772c02311b1b48858cac96a3
Noor Muhammad <connectwithnoor1@gmail.com>
2021-09-15 16:03:33 +0500

    update functionality

--- src/features/CheckCustomerEligibility/index.jsx
+++ src/features/CheckCustomerEligibility/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import InputField from '../../components/InputField';
 import { FaDollarSign } from 'react-icons/fa'
 import Button from '../../components/Button';
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCustomerEligibilityThunk } from './checkCustomerEligibilityThunk';
+import MarketingTag from './../../components/MarketingTag'
+import { BsStopwatch } from 'react-icons/bs'
+import { TiMessages } from 'react-icons/ti'
+// import { useHistory } from 'react-router-dom';
+
 import {
-    selectIsEligible, selectIsLoading, selectError
+    selectIsEligible, selectIsLoading
 } from './checkCustomerEligibilitySlice';
 
 const CheckCustomerEligibility = () => {
@@ -14,33 +19,47 @@ const CheckCustomerEligibility = () => {
     const { register, formState: { errors }, handleSubmit } = useForm({ criteriaMode: "all" });
 
     const dispatch = useDispatch();
-    const isEligibleSelector = useSelector(selectIsLoading);
-    const isLoading = useSelector(selectIsEligible);
-    const hasError = useSelector(selectError);
+    const isEligible = useSelector(selectIsEligible);
+    // const isLoading = useSelector(selectIsLoading);
+    // const history = useHistory()
+
 
     const onSubmit = formData => {
         dispatch(fetchCustomerEligibilityThunk(formData))
     }
 
+    // useEffect(() => {
+    //     if (isEligible !== null) {
+    //         if (isEligible) {
+    //             history.push('/register')
+    //         } else {
+    //             history.push('/disqualification')
+    //         }
+    //     }
+    // }, [isEligible, history])
+
+    // useEffect(() => {
+    //     showLoader
+    // }, [isLoading])
+
     return (
         <main className="flex justify-between items-center w-10/12 p-7 font-nunito h-full">
-            <section className="flex justify-between flex-col w-7/12 h-3/5 ">
-                <div className="font-extrabold text-4xl uppercase">
-                    <h1>Loan mate</h1>
-                    <div className="h-1 w-2/5 bg-gray-2x mt-3"></div>
-                </div>
+            <section className="flex flex-col justify-center w-7/12 h-3/5 ">
                 <div>
                     <h3 className="mb-6 font-bold text-3xl">Marketing Text Heading</h3>
                     <p className="text-xl font-medium leading-10 w-11/12 tracking-wide">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officiis iusto, deleniti quam sed
-                        nihil pariatur nam consequatur adipisci cumque perspiciatis velit dolores maiores placeat sint id porro
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. velit dolores maiores placeat sint id porro
                         est et ut distinctio fuga culpa cum consequuntur facere? Quaerat, quibusdam nobis. Lorem, ipsum dolor sit amet
                     </p>
                 </div>
+                <div className="mt-20">
+                    <MarketingTag Icon={BsStopwatch} title="Lorem Ipsum" description="dolor sit amet consectetur adipisicing elit. velit dolores maio" />
+                    <MarketingTag Icon={TiMessages} title="Lorem Ipsum" description="dolor sit amet consectetur adipisicing elit. velit dolores maio" />
+                </div>
             </section>
-            <section className="bg-gray-2x p-8 w-2/4 rounded-md shadow-xl">
-                <h3 className="border-b-2 border-gray-1x pb-2 font-bold text-4xl">Loan Form</h3>
-                <form className="pt-8" onSubmit={handleSubmit(onSubmit)}>
+            <section className="bg-gray-2x p-5 w-2/5 rounded-md shadow-xl">
+                <h3 className="border-b-2 border-gray-1x pb-2 font-bold text-3xl">Loan Form</h3>
+                <form className="pt-4" onSubmit={handleSubmit(onSubmit)}>
                     <InputField
                         labelFor="purchasePrice"
                         labelText="Auto Purchase Price"
