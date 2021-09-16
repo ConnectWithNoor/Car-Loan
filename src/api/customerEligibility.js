1bcc5ea86cb5398a0bc2f817f9bcc1d7871a613f
Noor Muhammad <connectwithnoor1@gmail.com>
2021-09-16 13:46:25 +0500

    release v1

--- src/api/customerEligibility.js
+++ src/api/customerEligibility.js
@@ -1,28 +1,47 @@
-import { DELAY_TIME, PURCHASE_PRICE_LIMIT, MINIMUM_CREDIT } from './../constant/constant';
+import {
+  DELAY_TIME,
+  PURCHASE_PRICE_LIMIT,
+  MINIMUM_CREDIT,
+} from './../constant/constant';
 
-export const customerEligibility = params => {
+export const customerEligibility = (params) => {
+  const oneFifthOfIncome = (yearlyIncome) => yearlyIncome / 5;
 
-    const oneFifthOfIncome = (yearlyIncome) => yearlyIncome / 5;
+  return new Promise((resolve, reject) => {
+    const { purchasePrice, autoMake, autoModel, yearlyIncome, creditScore } =
+      params;
 
-    return new Promise((resolve, reject) => {
-        const { purchasePrice, autoMake, autoModel, yearlyIncome, creditScore } = params;
+    if (
+      !purchasePrice ||
+      !autoMake ||
+      !autoModel ||
+      !yearlyIncome ||
+      !creditScore
+    ) {
+      return setTimeout(() => {
+        reject({
+          error: 'Please provide all the correct fields',
+          status: '400',
+        });
+      }, DELAY_TIME);
+    } else if (purchasePrice > PURCHASE_PRICE_LIMIT) {
+      return setTimeout(() => {
+        reject({
+          error: `Auto Purchase price can not be more than ${PURCHASE_PRICE_LIMIT}`,
+          status: '400',
+        });
+      }, DELAY_TIME);
+    } else if (
+      purchasePrice >= oneFifthOfIncome(yearlyIncome) ||
+      creditScore < MINIMUM_CREDIT
+    ) {
+      return setTimeout(() => {
+        resolve({ isEligible: false });
+      }, DELAY_TIME);
+    }
 
-        if (!purchasePrice || !autoMake || !autoModel || !yearlyIncome || !creditScore) {
-            return setTimeout(() => {
-                reject({ "error": "Please provide all the correct fields", "status": "400" })
-            }, DELAY_TIME)
-        } else if (purchasePrice > PURCHASE_PRICE_LIMIT) {
-            return setTimeout(() => {
-                reject({ "error": `Auto Purchase price can not be more than ${PURCHASE_PRICE_LIMIT}`, "status": "400" })
-            }, DELAY_TIME)
-        } else if (purchasePrice > oneFifthOfIncome(yearlyIncome) || creditScore < MINIMUM_CREDIT) {
-            return setTimeout(() => {
-                resolve({ "isEligible": false })
-            }, DELAY_TIME)
-        }
-
-        return setTimeout(() => {
-            resolve({ "isEligible": true })
-        }, DELAY_TIME)
-    })
-}
+    return setTimeout(() => {
+      resolve({ isEligible: true });
+    }, DELAY_TIME);
+  });
+};
